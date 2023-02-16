152adc5f044ea04d12feaa195d51d60bc4b71e6e
marceph <marcemtz321@gmail.com>
2023-02-16 19:58:42 -0300

    variables

--- src/main/java/com/ProyectoFinal/marceph/controller/CEducation.java
+++ src/main/java/com/ProyectoFinal/marceph/controller/CEducation.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("estudio")
-@CrossOrigin(origins = "https://marcephz.web.app") //https://marcephz.web.app  http://localhost:4200
+@CrossOrigin("*")//(origins = "https://marcephz.web.app") //https://marcephz.web.app  http://localhost:4200
 public class CEducation {
 
     @Autowired
